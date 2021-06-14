47da4d204065524b2b2750f685ae336ffb2e846b
DdiegoHD1998 <diego0433hd@gmail.com>
2021-06-14 17:48:03 -0400

    Detalles Carpeta service

--- Frontend/src/service/MesasService/MesaService.js
+++ Frontend/src/service/MesasService/MesaService.js
@@ -6,22 +6,22 @@ const baseUrl = `${process.env.REACT_APP_URL_BASE}/Mesas`
 export default class MesaService {
 
     async readAll(){
-        return await axios.get(baseUrl).then(res)
+        return await axios.get(baseUrl).then(res => res)
         .catch(err => err.response)
     }
 
     async create(mesa){
-        return await axios.post(baseUrl,mesa).then(res)
+        return await axios.post(baseUrl,mesa).then(res => res)
         .catch(err =>  err.response)
     }
 
     async update(mesa){
-        return await axios.put(`${baseUrl}/${mesa.idMesa}`, mesa).then(res)
+        return await axios.put(`${baseUrl}/${mesa.idMesa}`, mesa).then(res => res)
         .catch(err =>  err.response)
     }
 
     async delete(id){
-        return await axios.delete(`${baseUrl}/${id}`).then(res)
+        return await axios.delete(`${baseUrl}/${id}`).then(res => res)
         .catch(err =>  err.response)
     }
 
--- Frontend/src/service/UsuariosService/RolService.js
+++ Frontend/src/service/UsuariosService/RolService.js
@@ -6,22 +6,22 @@ const baseUrl = `${process.env.REACT_APP_URL_BASE}/Roles`
 export default class RolService {
 
     async readAll(){
-        return await axios.get(baseUrl).then(res)
+        return await axios.get(baseUrl).then(res => res)
         .catch(err => err.response)
     }
 
     async create(rol){
-        return await axios.post(baseUrl,rol).then(res)
+        return await axios.post(baseUrl,rol).then(res => res)
         .catch(err => err.response)
     }
 
     async update(rol){
-        return await axios.put(`${baseUrl}/${rol.idRol}`, rol).then(res)
+        return await axios.put(`${baseUrl}/${rol.idRol}`, rol).then(res => res)
         .catch(err => err.response)
     }
 
     async delete(id){
-        return await axios.delete(`${baseUrl}/${id}`).then(res)
+        return await axios.delete(`${baseUrl}/${id}`).then(res => res)
         .catch(err => err.response)
     }
 
